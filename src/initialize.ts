1b57e7f98682870366c09ae854874edcbee258a0
Peramanathan Sathyamoorthy <sathyam.peram@gmail.com>
2024-09-18 19:18:10 +0200

    Add comments for programtical access to containerized ollama failure from app
    Add graphite workflow screenshots

--- src/initialize.ts
+++ src/initialize.ts
@@ -1,7 +1,16 @@
 import { config } from '@dotenvx/dotenvx';
-config();
 
 import { Ollama } from 'ollama';
+
+config();
+
+// Need to rework, there is some issues
+// node app access ollama models which runs another container
+// Following error is thrown
+//    ConnectionRefused: Unable to connect. Is the computer able to access the url?
+//    amachat-1  |  path: "http://127.0.0.1:11434/api/tags"
+// the url is accessible from host browser, node REPL, and curl in terminal
+// Only programatically it fails
 
 let ollama = new Ollama();
 let modelList = await ollama.list();
