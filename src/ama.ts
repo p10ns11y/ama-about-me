160059d76bb7466e7662ad1e5e86c0c02e1fb2de
Peramanathan Sathyamoorthy <sathyam.peram@gmail.com>
2024-09-19 16:52:52 +0200

    debug and make the model pull successfull from app

--- src/ama.ts
+++ src/ama.ts
@@ -6,11 +6,15 @@ import { getEmbedding } from './embedding';
 import { getChromaVectorStore } from './chroma-vectorstore';
 import { chatAboutDocumentsContent } from './chat-about-documents-content';
 
+import { Ollama } from 'ollama';
+
+import http from 'http';
+
 async function main() {
   console.log('main....');
-  let embeddings = getEmbedding();
+  // let embeddings = getEmbedding();
 
-  await retrieveUsingInMemory(embeddings);
+  // await retrieveUsingInMemory(embeddings);
 
   // console.time('chat started');
   // await chatAboutDocumentsContent({
@@ -19,4 +23,36 @@ async function main() {
   // console.timeEnd('chat ended');
 }
 
-main();
+// main();
+
+let PORT = 3000;
+
+const ollamaBaseUrl = process.env.BASE_OLLAMA_URL || 'http://localhost:11434';
+
+async function setupOllama() {
+  try {
+    let ollama = new Ollama({
+      host: ollamaBaseUrl,
+    });
+    let modelList = await ollama.list();
+
+    console.log(modelList.models);
+
+    console.time('Pulling model');
+    await ollama.pull({ model: 'mxbai-embed-large' });
+    console.timeEnd('Pulling model');
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+const server = http.createServer((req, res) => {
+  void setupOllama();
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+  res.end('This is a simple Node.js server.\n');
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
--- src/embedding.ts
+++ src/embedding.ts
@@ -1,11 +1,12 @@
 import { OllamaEmbeddings } from '@langchain/ollama';
 
+const ollamaBaseUrl = process.env.BASE_OLLAMA_URL || 'http://localhost:11434';
+
 export function getEmbedding() {
   let embeddings = new OllamaEmbeddings({
     model: 'mxbai-embed-large',
+    baseUrl: ollamaBaseUrl,
   });
-
-  embeddings.model;
 
   return embeddings;
 }
